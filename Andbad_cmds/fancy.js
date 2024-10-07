19225d086c7860c92a3db7c2f5b8a98d7dd3cfdb
yessertech <badhacker057@gmail.com>
2024-10-07 21:07:53 +0300

    Update fancy.js

--- Andbad_cmds/fancy.js
+++ Andbad_cmds/fancy.js
@@ -8,7 +8,7 @@ zokou({ nomCom: "fancy", categorie: "Fun", reaction: "〽️" }, async (dest, zk
 
     try {
         if (id === undefined || text === undefined) {
-            return await repondre(`\nExemple : ${prefixe}fancy 10 Bmw-Md\n` + String.fromCharCode(8206).repeat(4001) + fancy.list('BMW-MD', fancy));
+            return await repondre(`\nExemple : ${prefixe}fancy 10 ZOMBIE KING\n` + String.fromCharCode(8206).repeat(4001) + fancy.list('role by yessertech', fancy));
         }
 
         const selectedStyle = fancy[parseInt(id) - 1];
