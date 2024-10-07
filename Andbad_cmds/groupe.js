1be18f07d37b7b7ac22603cb12cfd24e0bac2719
yessertech <badhacker057@gmail.com>
2024-10-07 21:13:02 +0300

    Update groupe.js

--- Andbad_cmds/groupe.js
+++ Andbad_cmds/groupe.js
@@ -31,7 +31,7 @@ zokou({ nomCom: "tagall", categorie: 'Group', reaction: "📣" }, async (dest, z
   let membresGroupe = verifGroupe ? await infosGroupe.participants : ""
   var tag = ""; 
   tag += `========================\n  
-        🌟 *Bmw-Md* 🌟
+        💀 *ZOMBIE-MD* 💀
 ========================\n
 👥 Group : ${nomGroupe} 🚀 
 👤 Autor : *${nomAuteurMessage}* 👋 
