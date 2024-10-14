ca227eb8df90e2cc9a637d83807eda7254380744
yessertech <badhacker057@gmail.com>
2024-10-14 22:52:03 +0300

    Update ping.js

--- Andbad_cmds/ping.js
+++ Andbad_cmds/ping.js
@@ -3,10 +3,10 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const { zokou } = require("../framework/zokou");
 zokou({ nomCom: "ping", reaction: "🧒", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
     console.log("Commande saisie !!!s");
-    let z = 'AM ALIVE STUPID.... \n\n\nGO GITHUB AND SEARCH ANDBAD MD V1 FORK THE REPO DEPLOY AND DM FOR YOURE 15$\n\n\n';
-    let d = '                                                                           I LOVE MY OWNER';
+    let z = 'AM ALIVE STUPID.... \n\n\nLIVE like you never live a gain🦍$\n\n\n';
+    let d = '                                                                           my owneris handsome';
     let varmess = z + d;
-    var img = 'https://i.imgur.com/L4U2K76.jpeg';
+    var img = 'https://i.imgur.com/ExqNrFC.jpeg';
     await zk.sendMessage(dest, { video: { url: img }, caption: varmess });
     //console.log("montest")
 });
